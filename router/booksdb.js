50e6661b24a5970afd38c6aaf10858e42fb23dfa
laila96 <madanilaila96@gmail.com>
2024-10-02 23:32:48 +0100

    Add files via upload

---
+++ router/booksdb.js
@@ -0,0 +1,12 @@
+let books = {
+      1: {"author": "Joanne Rowling1","title": "Harry Potter and the Sorcerers Stone", "reviews": {} },
+      2: {"author": "Joanne Rowling2","title": "Harry Potter and the Chamber of Secrets", "reviews": {} },
+      3: {"author": "Joanne Rowling3","title": "Harry Potter and the Prisoner of Azkaban", "reviews": {} },
+      4: {"author": "Joanne Rowling4","title": "Harry Potter and the Goblet of Fire", "reviews": {} },
+      5: {"author": "Joanne Rowling5","title": "Harry Potter and the Order of the Phoenix", "reviews": {} },
+      6: {"author": "Joanne Rowling6","title": "Harry Potter and the Half-Blood Prince", "reviews": {} },
+      7: {"author": "Joanne Rowling7","title": "Harry Potter and the Deathly Hallows", "reviews": {} },
+      8: {"author": "Joanne Rowling8","title": "Harry Potter and the Cursed Child", "reviews": {} }
+}
+
+module.exports=books;
